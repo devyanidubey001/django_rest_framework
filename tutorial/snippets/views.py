67b6dfe482771ea239cdf6ef9b9d9ba34de8687e
Devyani <devyanidubey97@gmail.com>
2022-06-10 16:17:54 +0700

    Adding Auth and Permissions

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,18 +1,37 @@
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 from rest_framework import generics
-# Create your views here.
+from django.contrib.auth.models import User
+from snippets.serializers import UserSerializer
+from rest_framework import permissions
+from snippets.permissions import IsOwnerOrReadOnly
+
 #adding format=None because no hardwired type of content and add support for format suffixes to our API endpoints
 #class based view
+
 class SnippetList(generics.ListCreateAPIView):
     """
     List all code snippets, or create a new snippet.
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+       serializer.save(owner=self.request.user)
 
     
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
